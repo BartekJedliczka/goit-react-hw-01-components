1375ff2c46aa9beed8143ce913e881f00b8811a4
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-10 16:50:50 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,27 +6,27 @@ import data from './statistics/data.json';
 
 export const App = () => {
   return (
-    <>
-      <Profile
-        avatar={user.avatar}
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-    </>
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    // >
-    //   React homework template
-    // </div>
+    // <>
+    //   <Profile
+    //     avatar={user.avatar}
+    //     username={user.username}
+    //     tag={user.tag}
+    //     location={user.location}
+    //     stats={user.stats}
+    //   />
+    //   <Statistics title="Upload stats" stats={data} />
+    // </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template wersja 2
+    </div>
   );
 };
