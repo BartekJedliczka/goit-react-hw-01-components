f2410fd2c2f6afa730be155ca8183209bd6f4b00
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-11 11:55:13 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import Profile from './profile/Profile';
 import user from './profile/user.json';
 
-// import Statistics from './statistics/Statistics';
-// import data from './statistics/data.json';
+import Statistics from './statistics/Statistics';
+import data from './statistics/data.json';
+
+// import FriendList from './friend-list/FriendList';
+// import friends from './friend-list/friends.json';
+
+// import TransactionHistory from './transaction/transactionHistory';
+// import transactions from './transaction/transactions.json';
 
 export const App = () => {
   return (
@@ -14,7 +20,9 @@ export const App = () => {
         location={user.location}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} /> */}
+      <Statistics title="Upload stats" stats={data} />
+      {/* <FriendList friends={friends} /> */}
+      {/* <TransactionHistory items={transactions} />; */}
     </>
   );
 };
