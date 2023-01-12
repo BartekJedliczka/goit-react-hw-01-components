10c62c4acced5563e1101782bef1453905053e3b
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-12 16:20:38 +0100

    Create FriendList.jsx

---
+++ src/components/friend-list/FriendList.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import FriendListItem from '../friend-list-items/FriendListItem';
+
+const FriendList = ({ friends }) => (
+  <ul className={css.friendList}>
+    {friends.map(({ isOnline, avatar, name, id }) => (
+      <FriendListItem
+        isOnline={isOnline}
+        avatar={avatar}
+        name={name}
+        key={id}
+      />
+    ))}
+  </ul>
+);
+
+FriendList.propTypes = {
+  isOnline: PropTypes.bool,
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.number,
+};
+
+export default FriendList;
