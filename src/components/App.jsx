313d53ff95ec0d407b2e827057a26d2b1fad3c86
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-17 19:23:15 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,8 @@ import data from './statistics/data.json';
 import FriendList from './FriendList/FriendList';
 import friends from './FriendList/friends.json';
 
-import TransactionHistory from './Transaction/transactionHistory';
-import transactions from './Transaction/transactions.json';
+import TransactionHistory from './transaction/transactionHistory';
+import transactions from './transaction/transactions.json';
 
 export const App = () => {
   return (
