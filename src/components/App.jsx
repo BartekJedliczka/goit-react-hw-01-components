e1a7eedf42e33681ec4d372b454755085915c3ad
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-17 21:38:32 +0100

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import user from '../data/user.json';
 import Statistics from './statistics/Statistics';
 import data from '../data/data.json';
 
-import FriendList from './friendList/FriendList';
+import FriendList from './FriendList/FriendList';
 import friends from '../data/friends.json';
 
 import TransactionHistory from './transaction/TransactionHistory';
