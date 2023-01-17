aeff552ef2d52b4f97b7b30d4d81e81a00c23a8b
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-17 23:01:06 +0100

    change transaction3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import data from '../data/data.json';
 import FriendList from './FriendList/FriendList';
 import friends from '../data/friends.json';
 
-import TransactionHistory from './transaction/transactionHistory';
+import TransactionHistory from './Transaction/TransactionHistory';
 import transactions from '../data/transactions.json';
 
 export const App = () => {
