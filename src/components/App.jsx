5e2eea28467df36f340f98db946c324cacaae22d
BartekJedliczka <bartekjedliczka@gmail.com>
2023-01-17 22:44:58 +0100

    change statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import Profile from './Profile/Profile';
 import user from '../data/user.json';
 
-import Statistics from './statistics/Statistics';
+import Statistics from './Statistics/Statistics';
 import data from '../data/data.json';
 
 import FriendList from './FriendList/FriendList';
